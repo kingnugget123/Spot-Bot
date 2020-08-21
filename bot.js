c3fba43eb83c006714ee59bdee5b09a8fd03e9bc
kingnugget123 <70015180+kingnugget123@users.noreply.github.com>
2020-08-21 16:13:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,11 +38,13 @@ else if(message.content.startsWith("?android") && message.channel.id === '745787
     else
     {
         message.reply('Your advertisement must be 50+ characters in length.');
+        delete(message);
     }
 }
 else
 {
 message.reply('Cannot advertise here.');
+delete(message);
 }
 });
 
