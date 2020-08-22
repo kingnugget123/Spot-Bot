12330673b7eb892ee21fe15685c438bfb94294e5
kingnugget123 <70015180+kingnugget123@users.noreply.github.com>
2020-08-22 22:27:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', msg => {
 
 	if(msg.author.bot) return;
 
-    if(msg.content.includes('discord.gg/' || 'discordapp.com/invite/') && msg.channel.id != commandchannel){
+    if(msg.content.includes('discord.gg/' || 'discordapp.com/invite/') && msg.channel.id != commandchannel.id){
 		 msg.delete();
 		 return message.reply('You cannot post server invites here!');
 	}
