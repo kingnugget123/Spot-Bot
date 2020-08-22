63a45e0ee1a9ffcc49e029c8bef298b595972715
kingnugget123 <70015180+kingnugget123@users.noreply.github.com>
2020-08-22 16:22:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('message', msg => {
 					messagewithoutcategory = msg.content.substring(6);
 				}
 				let AdvertisementPost = new Discord.MessageEmbed();
-				AdvertisementPost = new Discord.MessageEmbed().setAuthor('By @<' + msg.author.id + '>').setDescription(messagewithoutcategory).setTitle(adcategory);
+				AdvertisementPost = new Discord.MessageEmbed().setAuthor('By <@' + msg.author.id + '>').setDescription(messagewithoutcategory).setTitle(adcategory);
 				moderationchannel.send(AdvertisementPost).then(async messageto => {
 
 					await messageto.react('✅');
