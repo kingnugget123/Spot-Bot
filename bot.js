8e86735e696120c63109969d525a3f5427207bed
kingnugget123 <70015180+kingnugget123@users.noreply.github.com>
2020-08-22 16:39:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('message', msg => {
 					messagewithoutcategory = msg.content.substring(6);
 				}
 				let AdvertisementPost = new Discord.MessageEmbed();
-				AdvertisementPost = new Discord.MessageEmbed().setAuthor(msg.author).setDescription(messagewithoutcategory).setTitle(adcategory);
+				AdvertisementPost = new Discord.MessageEmbed().setAuthor(msg.author.id).setDescription(messagewithoutcategory).setTitle(adcategory);
 				moderationchannel.send(AdvertisementPost).then(async messageto => {
 
 					await messageto.react('✅');
@@ -109,7 +109,7 @@ client.on('messageReactionAdd', react => {
 		console.log(react.message.embeds[0].title);
 		const AdvertisementPost = new Discord.MessageEmbed();
       AdvertisementPost.setDescription(react.message.embeds[0].description);
-      AdvertisementPost.setAuthor(react.message.embeds[0].author);
+      AdvertisementPost.setAuthor('<@' + react.message.embeds[0].author + '>');
 
 		if(category == '?android') {
 
