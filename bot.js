b1e6a1c2093f3eb142e282e996e4ce36696b2e02
kingnugget123 <70015180+kingnugget123@users.noreply.github.com>
2020-08-22 15:47:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('message', msg => {
 					messagewithoutcategory = msg.content.substring(6);
 				}
 				let AdvertisementPost = new Discord.MessageEmbed();
-				AdvertisementPost = new Discord.MessageEmbed().setAuthor('By ' + msg.author).setDescription(messagewithoutcategory).setTitle(adcategory);
+				AdvertisementPost = new Discord.MessageEmbed().setAuthor('By @' + msg.author).setDescription(messagewithoutcategory).setTitle(adcategory);
 				moderationchannel.send(AdvertisementPost).then(async messageto => {
 
 					await messageto.react('✅');
@@ -104,7 +104,7 @@ client.on('messageReactionAdd', react => {
                // Time before delete
             }).catch('Error while deleting message');
          });
-         
+
 		const category = react.message.embeds[0].title;
 		console.log(react.message.embeds[0].title);
 		const AdvertisementPost = new Discord.MessageEmbed();
